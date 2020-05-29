f92404b1d28015fb50e8753f6472eed43123e16b
ojimenezl <ojimenezl@est.ups.edu.ec>
2020-05-29 10:47:09 -0500

    tercer commit colores

--- buscador/control.js
+++ buscador/control.js
@@ -3,6 +3,7 @@ const fs = require("fs"); // filesystem
 const est = require("./buscar");
 //const pag = require("./config/lista");
 let tareaPorHAcer = [];
+var dir = './resultados';
 
 const publicar = (file, country, year) => {
     est.getE(file, country, year).then(v => (v)).catch(msg => console.log(msg.message));
@@ -14,10 +15,17 @@ const guardar = (file, country, year) => {
 //Guardando en json
 const escribirjson = (country, year, vect) => {
     let data = (vect);
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir)
+    }
     fs.writeFile(`./resultados/${country}-${year}.txt`, data, (err) => {
-        if (err) throw new Error("No se pudo grabar", err);
+        if (err) {
+            throw new Error("No se pudo grabar", err);
+        } else {
+            console.log(`\nEl archivo se a guardado exitodamente:/resultados ${country}-${year}`.magenta);
+        }
     });
-    console.log(`\nEl archivo se a guardado exitodamente:/resultados ${country}-${year}`.magenta);
+
 
 };
 
